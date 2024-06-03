481f4c11ba560ba4c4f489962019b9ec28c47c8c
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2024-06-03 19:36:07 -0400

    finished all crud operations. todo list done

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,15 +1,58 @@
+import { useState, ChangeEvent  } from "react";
+import { ITodo } from "../src/interfaces"
+
 interface Props {
-  desc: String,
+  todo: ITodo;
+  deleteTodo(todoIdToDelete: number): void;
+  completeTodo(todoIdToComplete: number): void;
+  editTodo(todoIdToEdit: number): void;
 }
 
-function Todo( {desc}: Props) {
+function Todo( {todo, deleteTodo, completeTodo, editTodo}: Props) {
+  const [showEditor, setShowEditor] = useState<boolean>(false);
+  const [newDesc, setNewDesc] = useState<string>('');
+
+
+  const handleEvent = (event: ChangeEvent<HTMLInputElement>):void => {
+    setNewDesc(event.target.value)
+  }
+  const saveChange = ():void => {
+    if (newDesc !== '') {
+      todo.desc = newDesc
+    }
+    setShowEditor(false)
+  }
+
   return (
-    <div className='todo'>
-      <h3>{desc}</h3>
-      <button>Complete</button>
-      <button>Delete</button>
-      <button>Edit</button>
+    <>
+    {!showEditor ? (
+      <div className='todo'>
+        <h3>{todo.desc}</h3>
+        <button onClick={() => completeTodo(todo.id)}>Complete</button>
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        <button onClick={() => setShowEditor(true)}>Edit</button>
+      </div>
+    ) : (
+      <div className='todo'>
+        <input type="text" defaultValue={todo.desc} onChange={handleEvent}/>
+        <p></p>
+        <button onClick={saveChange}>Save</button>
+
+      </div>
+    )}
+    </>
+  )
+}
+
+interface CompleteProps {todo: ITodo}
+export const CompletedTodo = ( {todo}: CompleteProps ) => {
+  return (
+    <>
+    <div className="todo completed">
+      <h2>{todo.desc}</h2>
+      <h3>{todo.status}</h3>
     </div>
+    </>
   )
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import React, { useState, useEffect, FC } from 'react'
-import CreateTodoPanel from '../components/CreateTodoPanel'
-import TodoDisplay from '../components/TodoDisplay'
+import React, { useState, useEffect, FC, ChangeEvent } from 'react'
 import './App.css'
+import { ITodo } from './interfaces'
+import Todo, { CompletedTodo } from '../components/Todo'
 
 const LOREM = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate deserunt velit ducimus doloremque veritatis perferendis nulla laudantium inventore? Incidunt provident error rem quam molestiae iure odit repellat! Fugit, veniam error."
-
-// interface Todo {
-//   id: number;
-//   desc: string;
-// }
 
 // let todosList: Todo[] = [
 //   { id: 0, desc: LOREM },
 //   { id: 1, desc: LOREM },
 // ];
 
-// let todosCreated = 0
+let todosCreated = 0
 
 const App: FC = () => {
-  const [showWarning, setShowWarning] = useState<boolean>(true);
-  const [todoDesc, setTodoDesc] = useState<string>('');
-  const [todoList, setTodoList] = useState([]);
+  const [showCompleted, setShowCompleted] = useState<boolean>(true);
+  const [showInProgress, setShowInProgress] = useState<boolean>(true);
 
-  function createTodo() {
-    if (todoDesc !== '') {
-      console.log(todoDesc)
-      setShowWarning(false)
+  const [todo, setTodoDesc] = useState<string>('');
+  const [todoList, setTodoList] = useState<ITodo[]>([]);
+  const [completedList, setCompleteList] = useState<ITodo[]>([]);
+
+
+  const handleEvent = (event: ChangeEvent<HTMLInputElement>): void => {
+    setTodoDesc(event.target.value);
+  };
+
+  const addTodo = ():void => {
+    if (todo !== '') {
+      const newTodo = { id: todosCreated+=1, desc: todo, status: "incomplete"};
+      setTodoList([...todoList, newTodo]);
+      setTodoDesc('');
+      // console.log(todoList);
     } else {
-      console.log("todo must have a description")
-      setShowWarning(true)
+      console.log("todo must have a description");
     }
+  }
+
+  const deleteTodo = (todoIdToDelete: number): void => {
+    setTodoList(todoList.filter((todo) => {
+      return todo.id != todoIdToDelete;
+    }))
+  }
+  const completeTodo = (todoIdToComplete: number): void => {
+    const newCompletedTodo = todoList.filter((todo) => {
+      return todo.id == todoIdToComplete;
+    })[0];
+    deleteTodo(newCompletedTodo.id)
+    newCompletedTodo.status = 'completed'
+    
+    setCompleteList([...completedList, newCompletedTodo])
+  }
+  const editTodo = (todoIdToEdit: number): void => {
+
   }
 
   return (
@@ -37,14 +59,32 @@ const App: FC = () => {
       <div className='header'>
         <h1>Add Todo</h1>
         <h2>Description:</h2>
-        <input type="text" id="descBox" placeholder='Enter Todo' onChange={e => setTodoDesc(e.target.value)} />
-        {showWarning && <h3 className="warningLabel">Todo must have a description.</h3>}
-        <p></p>
-        <button onClick={() => createTodo()}>Create</button>
+        <input type="text" id="descBox" placeholder='Enter Todo' onChange={handleEvent} value={todo}/>
+        <button onClick={addTodo}>Create</button>
+
+        <div className="checkBoxes">
+          <input type="checkbox" id="inProgress" defaultChecked onChange={() => setShowInProgress(!showInProgress)}/>
+          <label htmlFor="inProgress"> In Progress </label>
+          <input type="checkbox" id="completed" defaultChecked onChange={() => setShowCompleted(!showCompleted)}/>
+          <label htmlFor="completed"> Completed </label>
+        </div>
       </div>
 
       <div className="todoList">
-        <TodoDisplay />
+        {showInProgress &&
+          <div className="inProgressTodos">
+            {todoList.map((todo: ITodo, key: number) => {
+              return <Todo key={key} todo={todo} deleteTodo={deleteTodo} completeTodo={completeTodo} editTodo={editTodo}/>
+            })}
+          </div>
+        }
+        { showCompleted && 
+        <div className="completeTodos">
+            {completedList.map((todo: ITodo, key: number) => {
+              return <CompletedTodo todo={todo} key={key}/>
+            })}
+        </div>
+        }
 
       </div>
 
